113d5a8036e8fdf4e4c86115f7014b91e3c59a01
NimeshBhevan <nimeshbhevan.ar@gmail.com>
2024-07-26 11:16:56 +0530

    some initializations....

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TailwindComponent } from './components/tailwind/tailwind.component';
+import { TaskComponent } from './components/task/task.component';
+import { LoginComponent } from './components/login/login.component';
+import { SingupComponent } from './components/singup/singup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TailwindComponent,
+    TaskComponent,
+    LoginComponent,
+    SingupComponent
   ],
   imports: [
     BrowserModule
